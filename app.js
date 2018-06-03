1e5228d345fd40b6179de73aadde9b6092c4a108
Prudhvi Kanteti <prudhvi@comitydesigns.com>
2018-06-03 19:43:46 -0400

    updated the db connection issue

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var monk = require('monk');
 //get the hook to db from mLab Mongo
 	// Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
 	var uri = 'mongodb://heroku_cwntx8n9:ttg2pro7osoaia4tm2pra6o74f@ds247690.mlab.com:47690/heroku_cwntx8n9';
-	mongodb.MongoClient.connect(uri, function(err, client) {
+	mongo.MongoClient.connect(uri, function(err, client) {
 		if(err) throw err;
 
 		var db = client.db('heroku_cwntx8n9');
